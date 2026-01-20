57f35d24e3b2a715a8db27e6e7c07bafe4be0c95
Rita A <zirizap.group@gmail.com>
2026-01-19 21:08:30 -0600

    Enable Django error logging for production

--- multiple_mapper/settings.py
+++ multiple_mapper/settings.py
@@ -152,3 +152,23 @@ FILE_UPLOAD_HANDLERS = [
 ]
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
